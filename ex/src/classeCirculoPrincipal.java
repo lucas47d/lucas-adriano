496c6d7140d2086cbd974926df5c3bcdb9349a10
Lucas <lucasnovelletto47@gmail.com>
2026-09-08 19:12:13 -0300

    Add files via upload

---
+++ ex/src/classeCirculoPrincipal.java
@@ -0,0 +1,11 @@
+public class classeCirculoPrincipal {
+        public static void main(String[] args) {
+
+            classeCirculo circulo1 = new classeCirculo();
+            circulo1.raio = 2;
+            System.out.println("Raio de "+circulo1.calcularArea());
+
+
+        }
+    }
+
