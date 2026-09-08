111598422631d4919d28fad945837b6c71870442
Lucas <lucasnovelletto47@gmail.com>
2026-09-08 20:56:45 -0300

    Add files via upload

---
+++ ex/src/ClasseUsuarioCorrigida.java
@@ -0,0 +1,9 @@
+public class ClasseUsuario {
+    public static void main(String[] args) {
+        ClasseFuncionarioPrincipal f1 = new ClasseFuncionarioPrincipal();
+        f1.identificacao = "123124124185";
+        f1.nomeFuncionario = "João";
+        f1.sobrenome = "Gabriel";
+        f1.salarioMensal = "1500";
+    }
+}
